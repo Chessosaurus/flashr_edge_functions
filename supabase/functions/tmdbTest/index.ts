6ecf9e8d6445679720ba03210e6bc9e653b58bfd
Fabian_Eilber <fabian.eilber01@gmail.com>
2024-04-03 14:14:07 +0200

    Function added to test the usage of tmdb

---
+++ supabase/functions/tmdbTest/index.ts
@@ -0,0 +1,22 @@
+async function genreList(req: Request): Promise<Response> {
+  const resp = await fetch("https://api.themoviedb.org/3/genre/tv/list", {
+    // The init object here has an headers object containing a
+    // header that indicates what type of response we accept.
+    // We're not specifying the method field since by default
+    // fetch makes a GET request.
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzM2ZDExMGY1YjRlMDY5NTc1ZDFiNzdiMTMzMmM2YSIsInN1YiI6IjY1ZmQ1OWI2MjI2YzU2MDE2NDZlZGMwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.81HUBUnUJvJD9qHxOxS7a0hkFumc3AqC6v52b8wWesM',
+      Host: 'api.themoviedb.org'
+    },
+  });
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+
+
+Deno.serve(genreList);
