2c68d24a9f660be58b5ac1a71417dd22fdc32e16
Fabian_Eilber <fabian.eilber01@gmail.com>
2024-04-05 12:39:21 +0200

    Functions concerning GenreStatus added

---
+++ supabase/functions/setGenreStatusNone/index.ts
@@ -0,0 +1,28 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.0"
+
+
+const supUrl = Deno.env.get("_SUPABASE_URL") as string;
+const supKey = Deno.env.get("_SUPABASE_KEY") as string;
+const supabase = createClient(supUrl, supKey);
+
+Deno.serve(async (req) => {
+
+  const body = await req.json();
+
+  const {user_id, genre_id} = body;
+
+  const { error } = await supabase
+  .from('GenreStatus')
+  .delete()
+  .eq('user_id', user_id)
+  .eq('genre_id', genre_id)
+        
+  const resp = {error}
+  
+  return new Response(
+    JSON.stringify( resp ),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+
---
+++ supabase/functions/setGenreStatusUninterested/index.ts
@@ -0,0 +1,28 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.0"
+
+
+const supUrl = Deno.env.get("_SUPABASE_URL") as string;
+const supKey = Deno.env.get("_SUPABASE_KEY") as string;
+const supabase = createClient(supUrl, supKey, {db: { schema: 'persistence' }});
+
+Deno.serve(async (req) => {
+
+  const {user_id, genre_id} = await req.json()
+
+  const { data, error } = await supabase
+  .from("GenreStatus")
+  .insert([
+    { user_id: user_id, genre_id: genre_id, status: 0 },
+  ])
+  .select()
+        
+
+  const resp = {data: data,error}
+  
+  return new Response(
+    JSON.stringify( resp ),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+
