8075193965ef8a7ff043f75141287d0ccaa810d6
Fabian_Eilber <fabian.eilber01@gmail.com>
2024-04-08 17:46:24 +0200

    Function added to get the trailer for a specific movie by its tmdb_id

---
+++ supabase/functions/getMovieTrailer/index.ts
@@ -0,0 +1,19 @@
+async function genreList(req: Request): Promise<Response> {
+
+  const { tmdb_id } = await req.json()
+
+  const resp = await fetch("https://api.kinocheck.de/movies?tmdb_id="+tmdb_id, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+
+
+Deno.serve(genreList);
