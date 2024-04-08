d4c98ea846e3976d8459c4c20e8253641eec53dd
Fabian_Eilber <fabian.eilber01@gmail.com>
2024-04-08 18:27:56 +0200

    Function added to get a list of all genres of movies and tv shows from the tmdb database.

--- supabase/functions/getAllGenres/index.ts
+++ supabase/functions/getAllGenres/index.ts
@@ -1,13 +1,28 @@
 async function getAllGenres(): Promise<Response> {
-  const resp = await fetch("https://api.themoviedb.org/3/genre/tv/list", {
+  const movieGenreResp = await fetch("https://api.themoviedb.org/3/genre/movie/list", {
     headers: {
       Accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzM2ZDExMGY1YjRlMDY5NTc1ZDFiNzdiMTMzMmM2YSIsInN1YiI6IjY1ZmQ1OWI2MjI2YzU2MDE2NDZlZGMwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.81HUBUnUJvJD9qHxOxS7a0hkFumc3AqC6v52b8wWesM',
       Host: 'api.themoviedb.org'
     },
   });
-  return new Response(resp.body, {
-    status: resp.status,
+
+  const tvGenreResp = await fetch("https://api.themoviedb.org/3/genre/tv/list", {
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzM2ZDExMGY1YjRlMDY5NTc1ZDFiNzdiMTMzMmM2YSIsInN1YiI6IjY1ZmQ1OWI2MjI2YzU2MDE2NDZlZGMwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.81HUBUnUJvJD9qHxOxS7a0hkFumc3AqC6v52b8wWesM',
+      Host: 'api.themoviedb.org'
+    },
+  });
+
+
+  const movieGenres = await movieGenreResp.json()
+  const tvGenres = await tvGenreResp.json()
+  
+  const mergedGenres = {movieGenres: movieGenres.genres, tvGenres: tvGenres.genres}
+
+  return new Response(JSON.stringify(mergedGenres), {
+    status:200,
     headers: {
       "content-type": "application/json",
     },
@@ -15,3 +30,9 @@ async function getAllGenres(): Promise<Response> {
 }
 
 Deno.serve(getAllGenres);
+
+
+export interface  IGenre {
+  id: number,
+  name: string
+}
